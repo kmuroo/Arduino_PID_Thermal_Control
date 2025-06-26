e2bf2472a4feaa0451c2a7138d1a3ed404722b17
kmuroo <kmuroo@gmail.com>
2025-06-26 17:17:22 +0900

    First uploard to remote repogitory
    First uploard to remote repogitory

---
+++ Arduino_PID_Thermal_Control/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Arduino_PID_Thermal_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 322);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(23, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Monitor ON";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.Location = new System.Drawing.Point(119, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Monitor OFF";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(119, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 35);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(119, 178);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 36);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "終了";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(23, 178);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "RESET";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(481, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "PID Thermal Controller Ver. 1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 389);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.MaximumSize = new System.Drawing.Size(682, 428);
+            this.MinimumSize = new System.Drawing.Size(682, 428);
+            this.Name = "Form1";
+            this.Text = "Arduino PID Thermal Control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Arduino_PID_Thermal_Control/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Application = System.Windows.Forms.Application;
+
+
+namespace Arduino_PID_Thermal_Control
+{
+    public partial class Form1 : Form
+    {
+        string default_portname = "COM7"; // デフォルトCOMポート
+        string[] ports;
+        bool monitor_enable = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosing += Form1_FormClosing;
+            add_serial_portname();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                comclose();
+            }
+            else
+            {
+                button4.BackColor = Color.LightGray;
+                button1.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void add_serial_portname()
+        {
+            ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+
+            if (comboBox1.FindString(default_portname) > 0)
+            {
+                comboBox1.SelectedIndex = comboBox1.FindString(default_portname);
+            }
+            serialPort1.PortName = comboBox1.SelectedItem.ToString();
+        }
+       
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private bool comopen()
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.BaudRate = 115200; // ArduinoソースのSerial　ボーレートと合わせる
+                    serialPort1.Open();
+                    while (serialPort1.IsOpen == false)
+                    {
+                        // ポートがオープンするまで待つ
+                    }
+                    textBox1.AppendText("PID thermal controllerをオープンしています\n\r\n\r");
+                                       
+                    serialPort1.ReadExisting(); //バッファを空に
+
+                    if (devicecheck() == 0) //PID thermal controllerかどうかのチェック
+                    {
+                        textBox1.AppendText("PID thermal controller を接続しました (" + serialPort1.PortName + ")\n\r\n\r");
+                        return true;
+                    }
+                    else {
+                        textBox1.AppendText("誤ったCOMポートを選択しています。他のCOMポートを試してください。\r\n\r\n");
+                        comclose();
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textBox1.AppendText(ex.Message);
+                    textBox1.AppendText("COMポートオープンに失敗しました。別のCOMポートを試してください。\r\n\r\n");
+                    comclose();
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        private int devicecheck()
+        {
+            try
+            {
+                textBox1.AppendText("device チェック開始\n\r\n\r");
+                string r = arduino_send_recv("r");
+                serialPort1.ReadExisting(); //バッファを空に
+                string a = "PID_Controller";
+                string c = arduino_send_recv("c");
+                c = c.TrimEnd('\r', '\n'); //改行コード削除
+                textBox1.AppendText("device チェック終了\n\r\n\r");
+                if (a == c) //接続成功すれば "PID_Controller" がArduinoから返ってくる
+                {
+                    return 0; //正しければ0を返す
+                }
+                else
+                {
+                    return 1; //間違っていれば1を返す
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.AppendText(ex.Message);
+                textBox1.AppendText("COMポートからが反応がありません\n\r\n\r");
+                comclose();
+                return -1;
+            }
+        }
+
+
+
+        private void comclose()
+        {
+
+            if (serialPort1.IsOpen == true)
+            {
+                arduino_send("r");
+                serialPort1.Close();
+                textBox1.AppendText("PID thermal controller を切断しました (" + serialPort1.PortName + ")\n\r\n\r");
+                button4.BackColor = Color.LightGray;
+                button1.BackColor = SystemColors.Control;
+                button3.BackColor = Color.LightGray;
+                button2.BackColor = SystemColors.Control;
+            }
+        }
+
+        private string arduino_send_recv(string send_message)//Arduinoにメッセージ送信、コールバックあり
+        {
+            string recv_message;
+
+            serialPort1.ReadExisting(); //バッファを空に
+            serialPort1.Write(send_message); //メッセージ送信
+
+            serialPort1.ReadTimeout = 5000;
+            recv_message = serialPort1.ReadLine();//メッセージ受信
+
+            return recv_message;
+        }
+
+        private void arduino_send(string send_message)//Arduinoにメッセージ送信、コールバックなし
+        {
+            serialPort1.Write(send_message);
+        }
+
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                if (comopen() == true)
+                {
+                    button1.BackColor = Color.LightGray;
+                    button4.BackColor = SystemColors.Control;
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (monitor_enable == false)
+            {
+                monitor_enable = true;
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.ReadExisting(); //バッファを空に
+                    arduino_send("e");
+                    textBox1.AppendText("Monitor start\n\r\n\r");
+                    button2.BackColor = Color.LightGray;
+                    button3.BackColor = SystemColors.Control;
+                }
+                else
+                {
+                    textBox1.AppendText("COMポ－トが開かれていません\n\r\n\r");
+                }
+                
+                Task<int> task = Task.Run(() => {
+                    return report_run();
+                });
+                
+            }
+        }
+
+        private int report_run()
+        {
+            while (monitor_enable)
+            {
+                textBox1.AppendText(serialPort1.ReadLine() + "\n\r");
+            }
+            arduino_send("s");
+            monitor_enable = false;
+            textBox1.AppendText("Monitor stop\n\r\n\r");
+            return 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (monitor_enable == true)
+            {
+                monitor_enable = false;
+                if (serialPort1.IsOpen)
+                {
+                    button3.BackColor = Color.LightGray;
+                    button2.BackColor = SystemColors.Control;
+                }
+                else
+                {
+                    textBox1.AppendText("COMポートが開かれていません\n\r\n\r");
+                }
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                if (monitor_enable == false)
+                {
+                    comclose();
+                }
+                else
+                {
+                    MessageBox.Show("Controller is now running");
+                }
+            }
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 質問ダイアログを表示する
+            DialogResult result = MessageBox.Show("終了しますか？", "質問", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                // はいボタンをクリックしたときはウィンドウを閉じる
+                e.Cancel = true;
+            }
+            else
+            {
+                //COMポートを閉じて終了
+
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                arduino_send("r");
+                MessageBox.Show("Controller RESET\n\r\n\r");
+                monitor_enable = false;
+                comclose();
+            }
+            else
+            {
+                textBox1.AppendText("ポートは開かれていません\n\r\n\r");
+            }
+        }
+    }
+}
